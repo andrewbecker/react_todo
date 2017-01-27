9ee105dab6e5f8ab813d7c0e8c4a0d11c5771be6
Andrew Becker <andrew.d.becker@lowes.com>
2017-01-27 12:44:30 -0500

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,41 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'A new task'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todo = [{
+        id: 1,
+        text: "Some task",
+        completed: false,
+        createdAt: 1,
+        completedAt: undefined
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+
+      var toggledTodo = reducers.todosReducer(df(todo), df(action));
+
+      expect(toggledTodo[0].completed).toEqual(true);
+      expect(toggledTodo[0].completedAt).toBeA('number');
+
+      toggledTodo = reducers.todosReducer(df(toggledTodo), df(action));
+
+      expect(toggledTodo[0].completed).toEqual(false);
+      expect(toggledTodo[0].completedAt).toBe(undefined);
+    });
+  });
 });
